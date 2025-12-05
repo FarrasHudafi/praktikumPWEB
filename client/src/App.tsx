1f001f6a34eca7bf05235f408bf1a031ccf1fa9d
FarrasHudafi <farras.qcp@gmail.com>
2025-12-05 20:35:12 +0700

    minor upd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,7 @@ import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
-export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
   return (
@@ -18,3 +17,4 @@ function App() {
 }
 
 export default App;
+  
